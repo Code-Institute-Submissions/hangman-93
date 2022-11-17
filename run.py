17cdf2805a8221c444231ff14f7b2638a31f5577
Forgottenit <forgottenit@hotmail.com>
2022-11-17 19:44:15 +0000

    Added a for loop to remove each instance of correctly chosen letter and a variable for turns in the game (lives)

--- run.py
+++ run.py
@@ -14,17 +14,28 @@ def main():
     word the computer has chosen and print message accordingly
     """
     print()
+    user_choices = []
+    lives = 5
     for letter in computer_word:
         print("_"+" ", end=" ")
-    user_choices = []
-    user_input = input("Enter a letter: ")
-    user_choices.append(user_input)
-    if user_input in computer_word:
-        print(f"Well Done {computer_word}")
-        print(user_choices)
-    else:
-        print(f"Well Done {computer_word}")
-        print(user_choices)
-        
+    while lives > 0:
+        user_input = input("Enter a letter: ")
+        if user_input in computer_word:
+            print(f"Well Done {computer_word}")
+            user_choices.append(user_input)
+            for user_input in computer_word:
+                computer_word.remove(user_input)
+            # If letter correct, remove it from list,
+            # then if word list empty, player won
+            print(user_choices)
+        else:
+            print(f"Hard luck {computer_word}")
+            user_choices.append(user_input)
+            print(user_choices)
+            lives -= 1
+            print(f"You have {lives} lives left")
+
+    print("Game over")
+
 
 main()
