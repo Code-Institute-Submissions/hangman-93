bdb07785109ff7d5c166d15a45e24a65cd21edd0
Forgottenit <forgottenit@hotmail.com>
2022-11-17 18:24:47 +0000

    Added used letters to a list and and converted word choice to list, changed print out to string literal

--- run.py
+++ run.py
@@ -4,21 +4,27 @@
 import random
 
 words = ["bob", "world", "hellooooooo"]
-computer_word =  random.choice(words)
+computer_word = list(random.choice(words))
+
 
 def main():
     """
-    test function to print dashes for amount of letters in a word, take a user input and check if in the 
+    test function to print dashes for amount of letters in a word,
+    take a user input and check if in the
     word the computer has chosen and print message accordingly
     """
     print()
-    for letter in computer_word:    
+    for letter in computer_word:
         print("_"+" ", end=" ")
-
+    user_choices = []
     user_input = input("Enter a letter: ")
+    user_choices.append(user_input)
     if user_input in computer_word:
-        print("Well Done "+computer_word)
+        print(f"Well Done {computer_word}")
+        print(user_choices)
     else:
-        print("Hard Luck " + computer_word)
+        print(f"Well Done {computer_word}")
+        print(user_choices)
+        
 
 main()
