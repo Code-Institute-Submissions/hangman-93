69f0b648c23ce9f586371950cc25ae4464652ea8
Forgottenit <forgottenit@hotmail.com>
2022-11-23 12:49:55 +0000

    Moved check on game being over to Class Game as a function

--- run2.py
+++ run2.py
@@ -5,11 +5,15 @@ user_choices = []
 correct_guesses = []
 images = ["Hanged", "_____", "two", "Three", "Four", "Five", "All lives left"]
 
-class Game:
 
+class Game:
+    """
+    Class that generates and displays computer choice of words and tests user input is valid
+    """
     def __init__(self):
         self.word = random.choice(words).upper()
         self.computer_word = list(self.word)
+        self.lives = 6
 
     def get_input(self):
         """
@@ -18,13 +22,17 @@ class Game:
         """
         while True:
             guess = input("\nEnter a letter: ").upper()
-            if guess.isalpha() and guess not in user_choices:
+            
+            if guess.isalpha() and guess not in user_choices and len(guess) == 1:
                 user_choices.append(guess)
                 return guess
             elif guess.isalpha() and guess in user_choices:
                 print("You've already picked that letter. Please try again.")
                 print(f"Correct letters : {set(correct_guesses)}")
                 print(f"user_choices are : {user_choices}")
+                continue
+            elif len(guess) > 1:
+                print("Sorry, that's too many letters")
                 continue
             else:
                 print("That's not a letter! Please try again")
@@ -40,6 +48,16 @@ class Game:
         print()
         print(" ".join(new_word))
 
+    def check_finished(self):
+        if len(self.computer_word) == 0:
+            print("Winner")
+            print("The word was : "+" "+self.word)
+            print(images[self.lives])
+        else:
+            print("Game over")
+            print("The Word was :" + self.word)
+            print(images[self.lives])
+
 
 def main():
     """
@@ -48,10 +66,10 @@ def main():
     word the computer has chosen and print message accordingly
     """
     print("\nWelcome to Hangman! \nYou have 6 attempts to guess the correct letters in the word")
-    lives = 6
     play = Game()
     play.display_word()
-    while lives > 0 and len(play.computer_word) != 0:
+    
+    while play.lives > 0 and len(play.computer_word) != 0:
         user_input = play.get_input()
         if user_input in play.computer_word:
             print(f"Well Done {user_input} was in the word!")
@@ -60,23 +78,16 @@ def main():
                 play.computer_word.remove(user_input)
             print(f"Correct letters : {set(correct_guesses)}")
             print(f"User Choices are : {user_choices}")
-            print(images[lives])
+            print(images[play.lives])
             play.display_word()
         else:
             print(f"\nHard luck {user_input} was not in the word...")
             print(f"User Choices are {user_choices}")
-            lives -= 1
-            print(f"You have {lives} lives left")
+            play.lives -= 1
+            print(f"You have {play.lives} lives left")
             play.display_word()
-            print(images[lives])
-    if len(play.computer_word) == 0:
-        print("Winner")
-        print("The word was : "+" "+play.word)
-        print(images[lives])
-    else:
-        print("Game over")
-        print("The Word was :" + play.word)
-        print(images[lives])
-
+            print(images[play.lives])
+    play.check_finished()
+   
 
 main()
